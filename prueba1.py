de39a92131b51c90241d333b5b3e685ded0515de
hellenik07 <hellenicoleherrerac@gmail.com>
2026-05-16 23:52:17 -0600

    Primer commit de IA

---
+++ prueba1.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import google.generativeai as genai
+
+#Api key y el modelo de gemini que voy a usar
+genai.configure(api_key="AIzaSyA-68xHre3Olf3MB2v2skAnyJB8jPuKUKk")
+model= genai.GenerativeModel('gemini-2.5-flash')
+
+#El diseño de la pagina
+st.title("Asistente Culinario")
+st.write("Escribe que quieres comer y te explicaré datos curiosos de esa comida")
+
+#Entradas del usuario
+platillo=st.text_input("Que se te antoja comer?")
+
+#Promp para gemini para que dé los datos curiosos
+
+if st.button("Explicar datos curiosos"):
+ 
+    reglasOcultas="""
+ Actúa como chef experto.
+ El usuario te dira que quiere comer. Tu objetivo NO es darle la receta.
+ debes darle DATOS CURIOSOS sobre la comida,quien lo inventó,en que año surgió,pais de origen,formas de comerlo.
+ utiliza un tono amigable.
+ La comida de quiere preparar es: """
+
+
+#aqui se junta lo que ponga el usuario junto con el prompt
+    mensajeFinal=reglasOcultas+platillo
+ #esta parte manda el mensaje a gemini
+    respuestaGemini=model.generate_content(mensajeFinal)
+#Esto es lo que se muestra
+    st.write(respuestaGemini.text)
